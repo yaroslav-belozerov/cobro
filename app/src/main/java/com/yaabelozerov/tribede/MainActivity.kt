9e23bd81ade6c2585c8586ca503ab65ac8965b97
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-28 20:45:00 +0300

    adjust ime padding

--- app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
+++ app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.yaabelozerov.tribede.ui.App
@@ -18,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    App(Modifier.padding(innerPadding))
+                    App(Modifier.padding(innerPadding).windowInsetsPadding(WindowInsets.ime))
                 }
             }
         }
