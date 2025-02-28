48bf34a55b7dc50751b211b75de3c3790615a8ba
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-28 17:02:13 +0300

    add deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -56,4 +57,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.coil)
+    implementation(libs.coil.network.ktor3)
+    implementation(libs.coil.compose)
+    implementation(libs.bundles.ktor)
 }
