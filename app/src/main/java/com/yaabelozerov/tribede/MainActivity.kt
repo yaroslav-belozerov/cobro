993a164660db566d4d136b833ee0562e8b694c57
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-28 17:15:04 +0300

    add app

--- app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
+++ app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.yaabelozerov.tribede.ui.App
 import com.yaabelozerov.tribede.ui.theme.TribedeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             TribedeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    App(Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TribedeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/yaabelozerov/tribede/ui/App.kt
@@ -0,0 +1,14 @@
+package com.yaabelozerov.tribede.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+}
