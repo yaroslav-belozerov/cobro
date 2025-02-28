2c3bb9badff41bda6f2ca713497cd431cad92a87
mnwok <man.nikeman.21@gmail.com>
2025-02-28 17:13:20 +0300

    colors

--- app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
+++ app/src/main/java/com/yaabelozerov/tribede/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.yaabelozerov.tribede.ui.theme.TribedeTheme
+import com.example.compose.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TribedeTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -38,10 +38,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TribedeTheme {
-        Greeting("Android")
-    }
-}
