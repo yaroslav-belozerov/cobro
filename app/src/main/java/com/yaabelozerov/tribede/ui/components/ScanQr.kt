8d2bd0cea5f383e57c9964d5de8ad1fe7a32bd67
mnwok <man.nikeman.21@gmail.com>
2025-03-02 17:00:52 +0300

    oh no nothing

--- app/src/main/java/com/yaabelozerov/tribede/ui/components/ScanQr.kt
+++ app/src/main/java/com/yaabelozerov/tribede/ui/components/ScanQr.kt
@@ -22,65 +22,52 @@ import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.yaabelozerov.tribede.domain.QrCodeAnalizer
 
-
 @Composable
-fun ScanQR(
-    modifier: Modifier,
-    hasCameraPermission: Boolean,
-    onScan: (String) -> Unit
-) {
+fun scanQR(modifier: Modifier, hasCameraPermission: Boolean, onScan: (String) -> Unit) {
 
-    val context = LocalContext.current
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(context)
-    }
-    val lifeCycleOwner = LocalLifecycleOwner.current
-    val selector1 = ResolutionSelector.Builder().setResolutionStrategy(ResolutionStrategy(Size(1280, 720), ResolutionStrategy.FALLBACK_RULE_NONE)).build()
-    Column(
-        modifier
-    ) {
-        if (hasCameraPermission) {
-            AndroidView(factory = { context ->
-                val previewView = PreviewView(context)
-                val preview = Preview.Builder().build()
-                val selector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-                preview.setSurfaceProvider(previewView.surfaceProvider)
-                val imageAnalysis = ImageAnalysis.Builder()
+  val context = LocalContext.current
+  val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+  val lifeCycleOwner = LocalLifecycleOwner.current
+  val selector1 =
+      ResolutionSelector.Builder()
+          .setResolutionStrategy(
+              ResolutionStrategy(Size(1280, 720), ResolutionStrategy.FALLBACK_RULE_NONE))
+          .build()
+  Column(modifier) {
+    if (hasCameraPermission) {
+      AndroidView(
+          factory = { context ->
+            val previewView = PreviewView(context)
+            val preview = Preview.Builder().build()
+            val selector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+            val imageAnalysis =
+                ImageAnalysis.Builder()
                     .setResolutionSelector(selector1)
                     .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
                     .build()
-                imageAnalysis.setAnalyzer(
-                    ContextCompat.getMainExecutor(context),
-                    QrCodeAnalizer { result ->
-                        onScan(result)
-                    }
-                )
-                try {
-                    cameraProviderFuture.get().bindToLifecycle(
-                        lifeCycleOwner,
-                        selector,
-                        preview,
-                        imageAnalysis
-                    )
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-                previewView
+            imageAnalysis.setAnalyzer(
+                ContextCompat.getMainExecutor(context), QrCodeAnalizer { result -> onScan(result) })
+            try {
+              cameraProviderFuture
+                  .get()
+                  .bindToLifecycle(lifeCycleOwner, selector, preview, imageAnalysis)
+            } catch (e: Exception) {
+              e.printStackTrace()
             }
-            )
-        }
-
+            previewView
+          })
     }
+  }
 }
 
-fun generateQRCode(data: String): Bitmap? {
-    return try {
-        val barcodeEncoder = BarcodeEncoder()
-        barcodeEncoder.encodeBitmap(data, BarcodeFormat.QR_CODE, 400, 400)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        null
-    }
+private fun generateQRCode(data: String): Bitmap? {
+  return try {
+    val barcodeEncoder = BarcodeEncoder()
+    barcodeEncoder.encodeBitmap(data, BarcodeFormat.QR_CODE, 400, 400)
+  } catch (e: Exception) {
+    e.printStackTrace()
+    null
+  }
 }
